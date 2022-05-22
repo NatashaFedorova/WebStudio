17e6b9fd4131dc92b675f34882bd2d099090962e
NatashaFedorova <dyadyafedor1287@gmail.com>
2022-05-22 16:10:31 +0300

    fix

--- js/no-scroll.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const refs = {
-    body: document.querySelector('.body'),
-  };
-  function toggleMenu() {
-    mobileMenuRef.classList.toggle('is-open');
-    refs.body.classList.toggle('no-scroll');
-  }
-})();
